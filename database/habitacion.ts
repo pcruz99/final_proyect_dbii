e2447081ddbbb7917445d5b73f85bd37e018f5f0
pcruz99 <69488209+pcruz99@users.noreply.github.com>
2023-01-29 01:23:14 -0500

    feat

--- database/habitacion.ts
+++ database/habitacion.ts
@@ -1,17 +1,17 @@
 import { IHabitacion } from "interfaces";
 const oracledb = require('oracledb');
-const mypw = 'hotel'  // set mypw to the hr schema password
+const mypw = 'cruz'  // set mypw to the hr schema password
 
 export async function habitacion() {
   try {
     var connection = await oracledb.getConnection({
-      user          : "hotel",
+      user          : "cruz_tarea",
       password      : mypw,
       connectString : "localhost"
     });
 
-    var result = await connection.execute(`SELECT last_name FROM employees`);
-    console.log("Result is:", result);
+    var result = await connection.execute(`SELECT * FROM producto`);
+    return result;
 
   } catch (err: any) {
     console.error(err.message);
--- src/pages/crud_habitacion/List.tsx
+++ src/pages/crud_habitacion/List.tsx
@@ -5,9 +5,28 @@ const List = () => {
   return (
     <>
       <ListHabitacion />
-     
     </>
   );
 };
 
 export default List;
+
+// You should use getServerSideProps when:
+// - Only if you need to pre-render a page whose data must be fetched at request time
+import { GetServerSideProps } from "next";
+import { habitacion } from "database";
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+
+  habitacion().then((req) =>{
+    console.log(req);
+    
+  })
+  
+  
+  
+
+  return {
+    props: {},
+  };
+};
